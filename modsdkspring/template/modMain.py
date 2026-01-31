567a60063fd3db616508be576a93fc8faa4a50b4
CreatorMC <2275381952@qq.com>
2026-01-31 21:52:21 +0800

    添加 isClientThread 和 isServerThread 方法

--- modsdkspring/template/modMain.py
+++ modsdkspring/template/modMain.py
@@ -1,31 +1,38 @@
 # -*- coding: utf-8 -*-
 
-from mod.common.mod import Mod
 import mod.client.extraClientApi as clientApi
 import mod.server.extraServerApi as serverApi
-from modCommon import modConfig
+from mod.common.mod import Mod
 from mod_log import logger
 
+from modCommon import modConfig
+from .plugins.MODSDKSpring.core.SystemInfo import SystemInfo
+
+
 @Mod.Binding(name=modConfig.MOD_NAMESPACE, version=modConfig.MOD_VERSION)
-class [MOD_NAME](object):
+class SpringTask(object):
 
     def __init__(self):
         logger.info("===== init %s mod =====", modConfig.MOD_NAMESPACE)
 
     @Mod.InitServer()
-    def [MOD_NAME]ServerInit(self):
+    def SpringTaskServerInit(self):
         logger.info("===== init %s server =====", modConfig.SERVER_SYSTEM_NAME)
+        from threading import current_thread
+        SystemInfo.serverThreadId = current_thread().ident
         serverApi.RegisterSystem(modConfig.MOD_NAMESPACE, modConfig.SERVER_SYSTEM_NAME, modConfig.SERVER_SYSTEM_CLS_PATH)
 
     @Mod.DestroyServer()
-    def [MOD_NAME]ServerDestroy(self):
+    def SpringTaskServerDestroy(self):
         logger.info("===== destroy %s server =====", modConfig.SERVER_SYSTEM_NAME)
-    
+
     @Mod.InitClient()
-    def [MOD_NAME]ClientInit(self):
+    def SpringTaskClientInit(self):
         logger.info("===== init %s client =====", modConfig.CLIENT_SYSTEM_NAME)
+        from threading import current_thread
+        SystemInfo.clientThreadId = current_thread().ident
         clientApi.RegisterSystem(modConfig.MOD_NAMESPACE, modConfig.CLIENT_SYSTEM_NAME, modConfig.CLIENT_SYSTEM_CLS_PATH)
     
     @Mod.DestroyClient()
-    def [MOD_NAME]ClientDestroy(self):
+    def SpringTaskClientDestroy(self):
         logger.info("===== destroy %s client =====", modConfig.CLIENT_SYSTEM_NAME)
